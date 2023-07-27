9dbecc08303ae1c4f49c351706240a0ff1368fca
Sean Darcy <darcys22@gmail.com>
2023-07-28 09:21:01 +1000

    remove header only options

--- cmake/Utils.cmake
+++ cmake/Utils.cmake
@@ -23,11 +23,6 @@ function(add_clang_format_target)
 						COMMAND ${${PROJECT_NAME}_CLANG_FORMAT_BINARY}
 						-i ${exe_sources} ${headers}
 						WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR})
-			elseif(${PROJECT_NAME}_BUILD_HEADERS_ONLY)
-				add_custom_target(clang-format
-						COMMAND ${${PROJECT_NAME}_CLANG_FORMAT_BINARY}
-						-i ${headers}
-						WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR})
 			else()
 				add_custom_target(clang-format
 						COMMAND ${${PROJECT_NAME}_CLANG_FORMAT_BINARY}
