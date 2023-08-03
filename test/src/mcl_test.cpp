3aa641f95784b36df03128f272e2cbfbbe7e6bbd
Sean Darcy <darcys22@gmail.com>
2023-08-03 16:31:14 +1000

    need to set mode for hash to G2

--- test/src/mcl_test.cpp
+++ test/src/mcl_test.cpp
@@ -22,6 +22,7 @@ struct BLSInitializer {
     BLSInitializer() {
         // init library once before calling the other APIs
         bls::init(mclBn_CurveSNARK1);
+        mclBn_setMapToMode(MCL_MAP_TO_MODE_TRY_AND_INC);
     }
     ~BLSInitializer() {
     }
@@ -90,62 +91,26 @@ TEST_CASE( "Weierstrass", "[mcl]" ) {
     std::array<unsigned char, 32> hash = utils::hash(message);
     mcl::bn::Fp x;
     x.setArrayMask(hash.data(), hash.size());
-    std::array<unsigned char, 64> serialized_hash;
-    uint8_t *hdst = serialized_hash.data();
-    mclSize serializedSignatureSize = 32;
     mcl::bn::Fp2 y = mcl::bn::Fp2(x,0);
-    if (y.a.serialize(hdst, serializedSignatureSize, mcl::IoSerialize | mcl::IoBigEndian) == 0)
-        throw std::runtime_error("size of x.a is zero");
-    if (y.b.serialize(hdst + serializedSignatureSize, serializedSignatureSize, mcl::IoSerialize | mcl::IoBigEndian) == 0)
-        throw std::runtime_error("size of x.b is zero");
-    std::array<unsigned char, 32> serialized_hashx;
-    uint8_t *hdstx = serialized_hashx.data();
-    if (y.a.serialize(hdstx, serializedSignatureSize, mcl::IoSerialize | mcl::IoBigEndian) == 0)
-        throw std::runtime_error("size of x.a is zero");
-    std::cout << __FILE__ << ":" << __LINE__ << " (" << __func__ << ") TODO sean remove this - hash sent to contract: " << utils::toHexString(serialized_hashx) << " - debug\n";
+    //std::cout << __FILE__ << ":" << __LINE__ << " (" << __func__ << ") TODO sean remove this - hash sent to contract: " << utils::toHexString(serialized_hashx) << " - debug\n";
     mcl::bn::Fp2 z;
     mcl::bn::G2::getWeierstrass(z, y);
-    std::array<unsigned char, 64> serialized_hash2;
-    uint8_t *hdst2 = serialized_hash2.data();
-    if (z.a.serialize(hdst2, serializedSignatureSize, mcl::IoSerialize | mcl::IoBigEndian) == 0)
-        throw std::runtime_error("size of x.a is zero");
-    if (z.b.serialize(hdst2 + serializedSignatureSize, serializedSignatureSize, mcl::IoSerialize | mcl::IoBigEndian) == 0)
-        throw std::runtime_error("size of x.b is zero");
     REQUIRE(mcl::bn::Fp2::squareRoot(z, z));
-    std::array<unsigned char, 64> serialized_hash3;
-    uint8_t *hdst3 = serialized_hash3.data();
-    if (z.a.serialize(hdst3, serializedSignatureSize, mcl::IoSerialize | mcl::IoBigEndian) == 0)
-        throw std::runtime_error("size of x.a is zero");
-    if (z.b.serialize(hdst3 + serializedSignatureSize, serializedSignatureSize, mcl::IoSerialize | mcl::IoBigEndian) == 0)
-        throw std::runtime_error("size of x.b is zero");
 
     mcl::bn::G2 Q;
     mcl::ec::tryAndIncMapTo<mcl::bn::G2>(Q, mcl::bn::Fp2(x,0));
-    std::vector<unsigned char> serialized_signature5(serializedSignatureSize*4);
-    uint8_t *dst5 = serialized_signature5.data();
-    if (Q.x.a.serialize(dst5, serializedSignatureSize, mcl::IoSerialize | mcl::IoBigEndian) == 0)
-        throw std::runtime_error("size of x.a is zero");
-    if (Q.x.b.serialize(dst5 + serializedSignatureSize, serializedSignatureSize, mcl::IoSerialize | mcl::IoBigEndian) == 0)
-        throw std::runtime_error("size of x.b is zero");
-    if (Q.y.a.serialize(dst5 + serializedSignatureSize * 2, serializedSignatureSize, mcl::IoSerialize | mcl::IoBigEndian) == 0)
-        throw std::runtime_error("size of y.a is zero");
-    if (Q.y.b.serialize(dst5 + serializedSignatureSize * 3, serializedSignatureSize, mcl::IoSerialize | mcl::IoBigEndian) == 0)
-        throw std::runtime_error("size of y.b is zero");
-    std::cout << __FILE__ << ":" << __LINE__ << " (" << __func__ << ") TODO sean remove this - " << utils::toHexString(serialized_signature5) << " - debug\n";
 
     mcl::bn::G2 Hm;
     bool r;
+    std::cout << __FILE__ << ":" << __LINE__ << " (" << __func__ << ") TODO sean remove this - X: " << utils::toHexString(x.getStr(mcl::IoSerialize | mcl::IoBigEndian)) << " - debug\n";
     mcl::bn::mapToG2(&r, Hm, mcl::bn::Fp2(x,0));
-    std::vector<unsigned char> serialized_signature6(serializedSignatureSize*4);
-    uint8_t *dst6 = serialized_signature6.data();
-    if (Hm.x.a.serialize(dst6, serializedSignatureSize, mcl::IoSerialize | mcl::IoBigEndian) == 0)
-        throw std::runtime_error("size of x.a is zero");
-    if (Hm.x.b.serialize(dst6 + serializedSignatureSize, serializedSignatureSize, mcl::IoSerialize | mcl::IoBigEndian) == 0)
-        throw std::runtime_error("size of x.b is zero");
-    if (Hm.y.a.serialize(dst6 + serializedSignatureSize * 2, serializedSignatureSize, mcl::IoSerialize | mcl::IoBigEndian) == 0)
-        throw std::runtime_error("size of y.a is zero");
-    if (Hm.y.b.serialize(dst6 + serializedSignatureSize * 3, serializedSignatureSize, mcl::IoSerialize | mcl::IoBigEndian) == 0)
-        throw std::runtime_error("size of y.b is zero");
-    std::cout << __FILE__ << ":" << __LINE__ << " (" << __func__ << ") TODO sean remove this - " << utils::toHexString(serialized_signature6) << " - debug\n";
+    REQUIRE(r);
+    Hm.normalize();
+    std::cout << __FILE__ << ":" << __LINE__ << " (" << __func__ << ") TODO sean remove this - HmXX: " << utils::toHexString(Hm.x.a.getStr(mcl::IoSerialize | mcl::IoBigEndian)) << " - debug\n";
+    std::cout << __FILE__ << ":" << __LINE__ << " (" << __func__ << ") TODO sean remove this - HmXY: " << utils::toHexString(Hm.x.b.getStr(mcl::IoSerialize | mcl::IoBigEndian)) << " - debug\n";
+    std::cout << __FILE__ << ":" << __LINE__ << " (" << __func__ << ") TODO sean remove this - HmYX: " << utils::toHexString(Hm.y.a.getStr(mcl::IoSerialize | mcl::IoBigEndian)) << " - debug\n";
+    std::cout << __FILE__ << ":" << __LINE__ << " (" << __func__ << ") TODO sean remove this - HmYY: " << utils::toHexString(Hm.y.b.getStr(mcl::IoSerialize | mcl::IoBigEndian)) << " - debug\n";
+    std::cout << __FILE__ << ":" << __LINE__ << " (" << __func__ << ") TODO sean remove this - HmZX: " << utils::toHexString(Hm.z.a.getStr(mcl::IoSerialize | mcl::IoBigEndian)) << " - debug\n";
+    std::cout << __FILE__ << ":" << __LINE__ << " (" << __func__ << ") TODO sean remove this - HmZY: " << utils::toHexString(Hm.z.b.getStr(mcl::IoSerialize | mcl::IoBigEndian)) << " - debug\n";
 
 }
